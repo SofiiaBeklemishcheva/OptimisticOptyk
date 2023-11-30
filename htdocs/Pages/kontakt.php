0d5743b7b4a64d2c761bae9f17c6fbc60513a61b
SofiiaBeklemishcheva <147977198+SofiiaBeklemishcheva@users.noreply.github.com>
2023-11-30 21:23:48 +0100

    zmiana 30.11.2023

---
+++ htdocs/Pages/kontakt.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Kontakt</title>
+    <link rel="stylesheet" href="../CSS/index.css">
+</head>
+<body>
+    <?php include ("./Layout/header.php");?>
+    <div class ="kontakt-information-container-zakopianka">
+
+
+        <video class = "road-to-shop-video-zakopianka" controls>
+
+            <source src="movie.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+            Your browser does not support the video tag.
+
+        </video>
+
+        <div class = "road-to-shop-video-zakopianka-title-container">
+            <p class = "road-to-shop-video-title-text">Centrum Handlowe "Zakopianka"</p>
+            <p class = "road-to-shop-video-shop-adres">ul. Zakopiańska 62, 30-418 Kraków</p>
+
+        </div>
+
+        <div class = "kontakt-information-links-container-zakopianka">
+
+</div>
+        
+    <div class ="kontakt-information-container-galeria-krakowska">
+
+        <video class = "road-to-shop-video-galeria-krakowska" controls>
+            <source src="movie.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+
+            Your browser does not support the video tag.
+        </video>
+
+        <div class = "road-to-shop-video-galeria-krakowska-title-container">
+            <p class = "road-to-shop-video-title-text">Galeria krakowska</p>
+            <p class = "road-to-shop-video-shop-adres">ul. Pawia 5, 31-154 Kraków</p>
+
+        </div>
+
+    </div>
+
+    </div>
+
+
+
+
+</body>
+</html>
