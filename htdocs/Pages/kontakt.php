ccb7761d616271a8fa62b94e500df03347952f18
SofiiaBeklemishcheva <147977198+SofiiaBeklemishcheva@users.noreply.github.com>
2023-12-02 12:51:31 +0100

    zmiany wersja 1 w dniu 02.12.2023

--- htdocs/Pages/kontakt.php
+++ htdocs/Pages/kontakt.php
@@ -2,51 +2,146 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Kontakt</title>
+    <title>Optimistic Optyk Kontakt</title>
     <link rel="stylesheet" href="../CSS/index.css">
 </head>
 <body>
     <?php include ("./Layout/header.php");?>
-    <div class ="kontakt-information-container-zakopianka">
+
+    //Informacje kontaktowe do punktu na Zakopiance w Krakowie
+
+    <div class = "infromation-about-shop-zakopianka-container"></div>
+    <div class = "video-road-to-shop-container">
+<video class = "video-road-to-shop" controls>
+  <source src="movie.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video>
+
+<div class = "shop-title-zakopianka">Centrum Handlowe Zakopianka </div>
+
+<a class = "google-maps-optimistic-optyk-zakopianka-link" href="https://www.google.com/maps/place/Optimistic+Optyk+Krak%C3%B3w+Zakopianka/@50.0139149,19.9297647,17z/data=!3m1!4b1!4m6!3m5!1s0x47165d80244d0799:0x3a11ce0ba145abc9!8m2!3d50.0139149!4d19.9323396!16s%2Fg%2F11tsjtlpqh?entry=ttu">
+<img class = "map-image-zakopianka" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-adres-zakopianka">ul. Zakopiańska 62, 30-418 Kraków</p>
+
+<a class = "instagram-optimistic-optyk-zakopianka-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "instagram-image-zakopianka" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-instagram-zakopianka">@optimistic.optyk.krakow</p>
+
+<a class = "facebook-optimistic-optyk-zakopianka-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "facebook-image-zakopianka" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-facebook-zakopianka">Optimistic OPTYK Kraków Zakopianka</p>
+
+<a class = "e-mail-optimistic-optyk-zakopianka-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "e-mail-image-zakopianka" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-e-mail-zakopianka">optimistic.krakow001@gmail.com</p>
+
+<a class = "phone-optimistic-optyk-zakopianka-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "phone-image-zakopianka" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-phone-number-zakopianka">518 114 935</p>
+
+</div> 
 
 
-        <video class = "road-to-shop-video-zakopianka" controls>
+//Informacje kontaktowe do punktu na Galerii Krakowskiej
+    
+    <div class = "infromation-about-shop-galeria-krakowska-container"></div>
+    <div class = "video-road-to-shop-container">
+<video class = "video-road-to-shop" controls>
+  <source src="movie.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video>
 
-            <source src="movie.mp4" type="video/mp4">
-            <source src="movie.ogg" type="video/ogg">
-            Your browser does not support the video tag.
+<div class = "shop-title-galeria-krakowska">Galeria Krakowska </div>
 
-        </video>
+<a class = "google-maps-optimistic-optyk-galeria-krakowska-link" href="https://www.google.com/maps/place/Optimistic+Optyk+Krak%C3%B3w+Zakopianka/@50.0139149,19.9297647,17z/data=!3m1!4b1!4m6!3m5!1s0x47165d80244d0799:0x3a11ce0ba145abc9!8m2!3d50.0139149!4d19.9323396!16s%2Fg%2F11tsjtlpqh?entry=ttu">
+<img class = "map-image-galeria-krakowska" src="https://via.placeholder.com/35x35" />
+</a>
 
-        <div class = "road-to-shop-video-zakopianka-title-container">
-            <p class = "road-to-shop-video-title-text">Centrum Handlowe "Zakopianka"</p>
-            <p class = "road-to-shop-video-shop-adres">ul. Zakopiańska 62, 30-418 Kraków</p>
+<p class = "kontakt-information-text-adres-galeria-krakowska">ul. Pawia 5, 31-154 Kraków</p>
 
-        </div>
+<a class = "instagram-optimistic-optyk-galeria-krakowska-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "instagram-image-galeria-krakowska" src="https://via.placeholder.com/35x35" />
+</a>
 
-        <div class = "kontakt-information-links-container-zakopianka">
+<p class = "kontakt-information-text-instagram">@optimistic.optyk.galeria.krk</p>
 
-</div>
-        
-    <div class ="kontakt-information-container-galeria-krakowska">
+<a class = "facebook-optimistic-optyk-galeria-krakowska-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "facebook-image-galeria-krakowska" src="https://via.placeholder.com/35x35" />
+</a>
 
-        <video class = "road-to-shop-video-galeria-krakowska" controls>
-            <source src="movie.mp4" type="video/mp4">
-            <source src="movie.ogg" type="video/ogg">
+<p class = "kontakt-information-text-facebook-galeria-krakowska">optimistic.optyk.galeria.krk</p>
 
-            Your browser does not support the video tag.
-        </video>
+<a class = "e-mail-optimistic-optyk-galeria-krakowska-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "e-mail-image-galeria-krakowska" src="https://via.placeholder.com/35x35" />
+</a>
 
-        <div class = "road-to-shop-video-galeria-krakowska-title-container">
-            <p class = "road-to-shop-video-title-text">Galeria krakowska</p>
-            <p class = "road-to-shop-video-shop-adres">ul. Pawia 5, 31-154 Kraków</p>
+<p class = "kontakt-information-text-e-mail-galeria-krakowska">optimistic.krakow002@gmail.com</p>
 
-        </div>
+<a class = "phone-optimistic-optyk-galeria-krakowska-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "phone-image-galeria-krakowska" src="https://via.placeholder.com/35x35" />
+</a>
 
-    </div>
+<p class = "kontakt-information-text-phone-number-galeria-krakowska">789 046 052</p>
 
-    </div>
+</div> 
 
+
+//Informacje kontaktowe do punktu w Wadowicach
+    
+    <div class = "infromation-about-shop-wadowice-container"></div>
+    <div class = "video-road-to-shop-container">
+<video class = "video-road-to-shop" controls>
+  <source src="movie.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video>
+
+<div class = "shop-title-wadowice">Zespół Zakładów Opieki Zdrowotnej w Wadowicach</div>
+
+<a class = "google-maps-optimistic-optyk-wadowice-link" href="https://www.google.com/maps/place/Optimistic+Optyk+Krak%C3%B3w+Zakopianka/@50.0139149,19.9297647,17z/data=!3m1!4b1!4m6!3m5!1s0x47165d80244d0799:0x3a11ce0ba145abc9!8m2!3d50.0139149!4d19.9323396!16s%2Fg%2F11tsjtlpqh?entry=ttu">
+<img class = "map-image-wadowice" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-adres-wadowice">Karmelicka 5, 34-100 Wadowice</p>
+
+<a class = "instagram-optimistic-optyk-wadowice-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "instagram-image-wadowice" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-informatiwadowice">@optimistic.optyk.wadowice</p>
+
+<a class = "facebook-optimistic-optyk-wadowice-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "facebook-image-wadowice" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-facebook-wadowice">optimistic.optyk.galeria.krk</p>
+
+<a class = "e-mail-optimistic-optyk-wadowice-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "e-mail-image-wadowice" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-e-mail-wadowice">optimistic.wadowice@gmail.com</p>
+
+<a class = "phone-optimistic-optyk-wadowice-link" href="https://www.instagram.com/optimistic.optyk.krakow/">
+<img class = "phone-image-wadowice" src="https://via.placeholder.com/35x35" />
+</a>
+
+<p class = "kontakt-information-text-phone-number-wadowice">780 124 090</p>
+
+</div> 
+    
 
 
 
